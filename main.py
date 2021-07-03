1f025e70e9b4a016e5657c57bcc1ddeb78ef1449
dieEisdiele <86861703+dieEisdiele@users.noreply.github.com>
2021-07-03 22:42:03 +1000

    first commit

---
+++ main.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import tcod
+
+from actions import EscapeAction, MovementAction
+from inputHandlers import EventHandler
+
+
+
+def main() -> None:
+    screenWidth = 80
+    screenHeight = 50
+
+    playerX = int(screenWidth/2)
+    playerY = int(screenHeight/2)
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu16x16_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    eventHandler = EventHandler()
+
+    with tcod.context.new_terminal(
+        screenWidth,
+        screenHeight,
+        tileset=tileset,
+        title="Dyskrasia",
+        vsync=False,
+    ) as context:
+        rootConsole = tcod.Console(screenWidth, screenHeight, order="F")
+        while True:
+            rootConsole.print(x=playerX, y=playerY, string="@")
+
+            context.present(rootConsole)
+
+            rootConsole.clear()
+
+            for event in tcod.event.wait():
+                action = eventHandler.dispatch(event)
+
+                if action is None:
+                    continue
+
+                if isinstance(action, MovementAction):
+                    playerX += action.dx
+                    playerY += action.dy
+
+                elif isinstance(action, EscapeAction):
+                    raise SystemExit
+
+
+
+if __name__ == "__main__":
+    main()
